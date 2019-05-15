955ad4ed948d630195485c908d23cc2ace67314a
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2019-05-15 20:32:59 +0200

    chore: add more scenarios

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import marked from 'marked';
+import createDOMPurify from 'dompurify';
+import { JSDOM } from 'jsdom';
+
+const DOMPurify = createDOMPurify(new JSDOM('').window);
 
 export default function parseReadme(readme: string): string | void {
   if (readme) {
-    marked.setOptions({
-      renderer: new marked.Renderer(),
-      sanitize: true
-    });
-    return marked(readme).trim();
+    return DOMPurify.sanitize(
+      marked(readme, {
+        sanitize: false
+      }).trim()
+    );
   }
 
   return;
