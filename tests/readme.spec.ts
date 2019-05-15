f421a83d5ee87c47f761e682f2f7da45e39ba90d
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2019-05-15 08:14:10 +0200

    chore: fix eslint

--- tests/readme.spec.ts
+++ tests/readme.spec.ts
@@ -1,33 +1,35 @@
 import parseReadme from '../src';
 
 describe('readme', () => {
-	describe('basic parsing', () => {
+  describe('basic parsing', () => {
     test('should parse basic', () => {
-			expect(parseReadme("# hi")).toEqual(`<h1 id=\"hi\">hi</h1>`);
-		});
+      expect(parseReadme('# hi')).toEqual(`<h1 id=\"hi\">hi</h1>`);
+    });
 
-		test('should parse basic / js alert', () => {
-			expect(parseReadme("[Basic](javascript:alert('Basic'))")).toEqual("<p>Basic</p>")
-		});
+    test('should parse basic / js alert', () => {
+      expect(parseReadme("[Basic](javascript:alert('Basic'))")).toEqual('<p>Basic</p>');
+    });
 
-		test('should parse basic / local storage', () => {
-			expect(parseReadme("[Local Storage](javascript:alert(JSON.stringify(localStorage)))"))
-				.toEqual("<p>Local Storage</p>");
-		});
+    test('should parse basic / local storage', () => {
+      expect(parseReadme('[Local Storage](javascript:alert(JSON.stringify(localStorage)))')).toEqual('<p>Local Storage</p>');
+    });
 
-		test('should parse basic / case insensitive', () => {
-			expect(parseReadme("[CaseInsensitive](JaVaScRiPt:alert('CaseInsensitive'))"))
-				.toEqual("<p>CaseInsensitive</p>");
-		});
+    test('should parse basic / case insensitive', () => {
+      expect(parseReadme("[CaseInsensitive](JaVaScRiPt:alert('CaseInsensitive'))")).toEqual('<p>CaseInsensitive</p>');
+    });
 
-		test('should parse basic / url', () => {
-			expect(parseReadme("[URL](javascript://www.google.com%0Aalert('URL'))"))
-				.toEqual("<p>URL</p>");
-		});
+    test('should parse basic / url', () => {
+      expect(parseReadme("[URL](javascript://www.google.com%0Aalert('URL'))")).toEqual('<p>URL</p>');
+    });
 
-		test('should parse basic / in quotes', () => {
-			expect(parseReadme("[In Quotes]('javascript:alert(\"InQuotes\")')"))
-				.toEqual("<p>In Quotes</p>");
-		});
-	});
+    test('should parse basic / in quotes', () => {
+      expect(parseReadme('[In Quotes](\'javascript:alert("InQuotes")\')')).toEqual('<p>In Quotes</p>');
+    });
+  });
+
+  describe('images', () => {
+    test('should parse basic / in quotes', () => {
+      expect(parseReadme('![Escape SRC - onload](https://www.example.com/image.png"onload="alert(\'ImageOnLoad\'))')).toEqual('<p><img src="https://www.example.com/image.png%22onload=%22alert(\'ImageOnLoad\')" alt="Escape SRC - onload"></p>');
+    });
+  });
 });
